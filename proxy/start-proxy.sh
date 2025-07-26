a5852d5cf36e5fac44d9281ade692104f04893d3
serious <seriouz@gmail.com>
2025-07-27 00:20:33 +0200

    param

--- proxy/start-proxy.sh
+++ proxy/start-proxy.sh
@@ -5,4 +5,4 @@ LISTEN_PORT=$2
 
 echo "Starting proxy for port ${LISTEN_PORT} -> ${HOST}:1${LISTEN_PORT}"
 socat -v -v TCP4-LISTEN:${LISTEN_PORT},fork,reuseaddr \
-    SYSTEM:"bash -c '/set-container-status.sh unpause >&2; exec socat -v -v STDIO TCP4:${HOST}:1${LISTEN_PORT}'" &
+    SYSTEM:"bash -c '/set-container-status.sh unpause ${HOST} >&2; exec socat -v -v STDIO TCP4:${HOST}:1${LISTEN_PORT}'" &

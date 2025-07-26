96f804474257e79adc16069c2ce515c234a557b4
serious <seriouz@gmail.com>
2025-07-27 00:22:51 +0200

    docker port

--- proxy/start-proxy.sh
+++ proxy/start-proxy.sh
@@ -3,6 +3,6 @@
 HOST=$1
 LISTEN_PORT=$2
 
-echo "Starting proxy for port ${LISTEN_PORT} -> ${HOST}:1${LISTEN_PORT}"
+echo "Starting proxy for port ${LISTEN_PORT} -> ${HOST}:${LISTEN_PORT}"
 socat -v -v TCP4-LISTEN:${LISTEN_PORT},fork,reuseaddr \
-    SYSTEM:"bash -c '/set-container-status.sh unpause ${HOST} >&2; exec socat -v -v STDIO TCP4:${HOST}:1${LISTEN_PORT}'" &
+    SYSTEM:"bash -c '/set-container-status.sh unpause ${HOST} >&2; exec socat -v -v STDIO TCP4:${HOST}:${LISTEN_PORT}'" &
